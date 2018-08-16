af90dd8817f4029a107253a8e42cfd038c5c5c56
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-16 16:50:15 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,6 +9,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
+    binding.pry
     @landmark = Landmark.create(params[:landmark])
     @landmark.save
     binding.pry
