b3b27da4131700d3f37dc15c8be2553a4d20d2cc
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-16 17:13:32 +0000

    automatically backed up by learn

--- spec/landmarks_controller_view_spec.rb
+++ spec/landmarks_controller_view_spec.rb
@@ -65,7 +65,6 @@ describe LandmarksController do
     visit "/landmarks/#{@original_landmark.id}/edit"
     fill_in :name, with: "BQE!!!!"
     fill_in :year_completed, with: 9999
-    binding.pry
     click_button "Edit Landmark"
 
     expect(page.body).to include("BQE!!!!")
