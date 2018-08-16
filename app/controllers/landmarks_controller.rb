e475ed74da8d885ec5266729b45fc3c8bb064f8b
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-16 17:18:32 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,14 +10,14 @@ class LandmarksController < ApplicationController
     erb :'landmarks/new'
   end
 
-  get '/landmarks/:id/edit' do
-    @landmark = Landmark.find(params[:id])
-    erb :'/landmarks/edit'
-  end
-
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
   end
 
   post '/landmarks/:id' do
