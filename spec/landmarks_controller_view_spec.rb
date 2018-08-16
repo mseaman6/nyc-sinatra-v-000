0f9020443f59f95444984cf91783b62cecde4540
Monica Seaman <monica.l.seaman@gmail.com>
2018-08-16 16:49:31 +0000

    automatically backed up by learn

--- spec/landmarks_controller_view_spec.rb
+++ spec/landmarks_controller_view_spec.rb
@@ -25,7 +25,6 @@ describe LandmarksController do
     fill_in :landmark_name, :with => "Arc de Triomphe"
     fill_in :landmark_year_completed, :with => 1806
     click_button "Create New Landmark"
-    binding.pry
     expect(Landmark.all.count).to eq(2)
   end
 
